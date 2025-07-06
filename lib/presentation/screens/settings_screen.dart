13e6d472461d1473afe9744e93d6051d2858bd38
Hikusama <leomessi25k@gmail.com>
2025-07-06 11:37:23 +0800

    flutter_secure_storage

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final ipTextController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Settings Screen', style: TextStyle(fontSize: 24)));
+    return Column(
+      children: [
+        Text('Set the host ip'),
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'First Name',
+            labelStyle: TextStyle(fontSize: 12),
+            hintText: 'Enter a first name',
+            border: OutlineInputBorder(),
+            isDense: true,
+            contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+          ),
+          style: TextStyle(fontSize: 12, color: Colors.black),
+          controller: ipTextController,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter the first name';
+            }
+            if (value.length > 20) {
+              return 'Use only 20 characters';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
   }
 }
