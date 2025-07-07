45e6112856d6bd57d43e35c4a7cac83fd21d4bae
Hikusama <leomessi25k@gmail.com>
2025-07-08 00:10:10 +0800

    form login file and logged in file

---
+++ lib/data/view/logged_in.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class LoggedIn extends StatefulWidget {
+  const LoggedIn({super.key});
+
+  @override
+  State<LoggedIn> createState() => _LoggedInState();
+}
+
+class _LoggedInState extends State<LoggedIn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
